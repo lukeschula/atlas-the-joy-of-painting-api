b9aab7c26de8fb3fbc1c207c4120e4d3f8724d14
lukeschula <schulaluke@gmail.com>
2024-12-01 17:58:03 -0600

    Database created

---
+++ sql/createDatabase.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS joy_of_painting;
+USE joy_of_painting;
+
+CREATE TABLE IF NOT EXISTS Episodes (
+  episode_id INT AUTO_INCREMENT PRIMARY KEY,
+  episode_number VARCHAR(50),
+  episode_title VARCHAR(255),
+  season INT,
+  episode INT,
+  month VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Colors (
+  color_id INT AUTO_INCREMENT PRIMARY KEY,
+  color_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Episode_Color (
+  episode_color_id INT AUTO_INCREMENT PRIMARY KEY,
+  episode_id INT,
+  color_id INT,
+  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id),
+  FOREIGN KEY (color_id) REFERENCES Colors(color_id)
+);
+
+CREATE TABLE IF NOT EXISTS Subjects (
+  subject_id INT AUTO_INCREMENT PRIMARY KEY,
+  subject_name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS Episode_Subject (
+  episode_subject_id INT AUTO_INCREMENT PRIMARY KEY,
+  episode_id INT,
+  subject_id INT,
+  FOREIGN KEY (episode_id) REFERENCES Episodes(episode_id),
+  FOREIGN KEY (subject_id) REFERENCES Subjects(subject_id)
+);
