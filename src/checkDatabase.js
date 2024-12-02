2971e445fd2a353425f18ba6f0614502a16095e7
lukeschula <schulaluke@gmail.com>
2024-12-01 19:00:15 -0600

    Check database, and parse data

---
+++ src/checkDatabase.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql');
+
+function checkDatabaseExists() {
+  const db = createDBConnectionWithoutDB();
+
+  return new Promise((resolve, reject) => {
+    // Query to check if database exists
+    db.query("SHOW DATABASES LIKE 'joy_of_painting'", (error, results, fields) => {
+      if (error) reject(error);
+
+      // Close the database connection
+      db.end();
+
+      // If database does not exist, resolve promise with false, else resolve with true
+      results.length === 0 ? resolve(false) : resolve(true);
+    });
+  });
+}
+
+function createDBConnectionWithoutDB() {
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '7432',
+    multipleStatements: true
+  });
+
+  connection.connect((error) => {
+    if (error) {
+      console.error('Error connecting to database: ' + error.stack);
+      return;
+    }
+    console.log('Connected to database as id ' + connection.threadId);
+  });
+
+  return connection;
+}
+
+module.exports = { checkDatabaseExists, createDBConnectionWithoutDB };
